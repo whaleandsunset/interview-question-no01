906aed017a98e4f1856ecf98d63d85cbaf97cf5a
whaleandsunset <168166276+whaleandsunset@users.noreply.github.com>
2026-08-03 16:53:30 +0700

    ทดสอบ Frontend หลังจากแก้ไข SSR

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommentComponent } from './components/comment/comment.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [CommentComponent],
+  template: `<app-comment></app-comment>`
 })
-export class App {
-  protected readonly title = signal('Frontend');
-}
+export class App {}
